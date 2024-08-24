e39ca2360fa45e116a774373483676abab0eb45b
Kwasiob <100800474+Kwasiob@users.noreply.github.com>
2024-08-24 23:26:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(cors());
 
 const mailOptions = {
-  from: "koobiri@st.ug.edu.gh",
-  to: "obirikwasi@gmail.com",
+  from: "eyeboah114@st.ug.edu.gh",
+  to: "ichabordyeboah247@gmail.com",
   subject: "CMS Notification",
   html: "<h1>Message sent</h1>",
 };
